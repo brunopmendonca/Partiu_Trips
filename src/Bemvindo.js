a8374a4bed3e06ed3570a15f8dc4a296e42ef468
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2021-03-02 22:49:35 -0300

    bem vindo

--- src/Bemvindo.js
+++ src/Bemvindo.js
@@ -12,59 +12,26 @@ const dados = require('../icons/praia.png')
   
 
 
-const Bemvindo = () =>{
+const Bemvindo = ({route}) =>{
 
-    
+ 
+     
   const navigation= useNavigation()
-  const [pic,setImagem] = useState([
-    
-    {
-      title:"O Justiceiro",
-      text: "Após o assassinato de sua família, Frank Castle está traumatizado e sendo caçado. No submundo do crime, ele se tornará aquele conhecido como O Justiceiro",
-      release: 2018,
-      img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/background.jpg'
-  },
-  {
-      title:"Bad Boys for life",
-      text: "Terceiro episódio das histórias dos policiais Burnett (Martin Lawrence) e Lowrey (Will Smith), que devem encontrar e prender os mais perigosos traficantes de drogas da cidade.",
-      release: 2020,
-      img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/badboy.jpg'
-  },
-  {
-      title:"Viúva Negra",
-      text: "Em Viúva Negra, após seu nascimento, Natasha Romanoff (Scarlett Johansson) é dada à KGB, que a prepara para se tornar sua agente definitiva.",
-      release: 2020,
-      img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/blackwidow.jpg'
-  },
-  {
-      title:"Top Gun: MAVERICK",
-      text: "Em Top Gun: Maverick, depois de mais de 30 anos de serviço como um dos principais aviadores da Marinha, o piloto à moda antiga Maverick (Tom Cruise) enfrenta drones e prova que o fator humano ainda é fundamental no mundo contemporâneo das guerras tecnológicas.",
-      release: 2020,
-      img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/topgun.jpeg'
-  },
-  {
-      title:"BloodShot",
-      text: "Bloodshot é um ex-soldado com poderes especiais: o de regeneração e a capacidade de se metamorfosear. ",
-      release: 2020,
-      img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/blood.jpg'
-  },
-  {
-      title:"Free Guy",
-      text: "Um caixa de banco preso a uma entediante rotina tem sua vida virada de cabeça para baixo quando ele descobre que é personagem em um brutalmente realista vídeo game de mundo aberto.",
-      release: 2020,
-      img: 'https://sujeitoprogramador.com/wp-content/uploads/2020/05/freeguy.jpg'
-  },
+  const [pic,setImagem] = useState(route.params) 
   
-  ])
-
+  
+  const [dados, setDados]= useState([])
+  dados.push(pic)
   const carosselRef = useRef()
-  
+  console.log(dados)
 
   const renderItem = ({item, index}) => {
     return (
         <View style={{flex:1}}>
-            <Text style={style.title}>{ item.title }</Text>
-            <Image source= {{uri: item.img}} style={{overflow:"visible", flex:1,}} />
+            <Text style={style.title}>Ida: { item.ida }</Text>
+            <Text style={style.title}>Volta: { item.volta }</Text>
+    <Text style={style.title}>{ pic.length}</Text>
+            <Image source= {item.imagem} style={{overflow:"visible", flex:1,}} />
         </View>
     );
 }
@@ -92,7 +59,7 @@ const Bemvindo = () =>{
             <Carousel
                     style={{height:200, width:500}}
                     ref={carosselRef}
-                    data={pic}
+                    data={dados}
                     renderItem={renderItem}
                     sliderWidth= {300}
                     itemWidth = {300}
