bcddb34bd890413a3e0a5b893215b237ec211361
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2021-03-04 22:21:43 -0300

    funcionalidades

--- src/Func.js
+++ src/Func.js
@@ -1,27 +1,53 @@
 import React, { useState} from 'react';
 import {  Text, View, StyleSheet, Image, TextInput, ScrollView  } from 'react-native';
-import {Button} from 'react-native-elements'
+// import {Button} from 'react-native-elements'
 import { TouchableHighlight } from 'react-native-gesture-handler';
 import firebase from '../Firebaseconection'
-import { Appbar } from 'react-native-paper';
+import { Appbar, Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
 import { Platform } from 'react-native';
 
-const Func = ({navigation}) =>{
+const Func = ({navigation, route}) =>{
   
+  const LeftContent = props => <Avatar.Icon {...props} icon="folder" />
+  const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
+  const [ida, setIda]= useState(route.params.ida)
+  const [volta, setVolta]= useState(route.params.volta)
+  const [imagem, setImagem]= useState(route.params.imagem)
+
+
 
 
 return ( 
   
 
-<View>
-  
-  <Appbar.Header>
-       <Appbar.Content title="Title" subtitle={'Subtitle'} />
-        <Appbar.Action icon="magnify" onPress={() => {}} />
-        {/* <Appbar.Action icon={MORE_ICON} onPress={() => {}} /> */}
-    </Appbar.Header>
 
-</View>
+<ScrollView>
+      <View style= {style.header} >
+          <Image source={require("../icons/simbolo.png")}/>
+          <Text style={{fontSize:28, color:"#1E7987", textAlign:"center"}}>Viagem</Text>
+          <Appbar.Action icon={MORE_ICON} onPress={() => {}} />
+      </View>
+
+     
+         
+
+      <Card>
+          <Card.Title title="Card Title" subtitle="Card Subtitle" left={LeftContent} />
+          <Card.Content>
+            <Title>Card title</Title>
+            <Paragraph>Card content</Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+          <Card.Actions>
+            <Button>Cancel</Button>
+            <Button>Ok</Button>
+          </Card.Actions>
+      </Card>
+     
+
+</ScrollView>
+
+
 
 
 )};
@@ -32,23 +58,18 @@ const style = StyleSheet.create({
 
   header:{
     flexDirection:"row",
-    height:"15%",
     alignItems:"baseline",
     justifyContent:"space-around",
-    paddingTop:30
 
    },
-
-  
+ 
   simbolo:{
-        
-    
+      
     alignItems:"baseline",
     justifyContent:"center",
     alignItems:"baseline",
 
 },
-
 
   titulo:{
     height:"15%",
@@ -56,6 +77,18 @@ const style = StyleSheet.create({
     justifyContent:"center",
     
   },
+
+  viagem:{
+    
+    
+  },
+
+  imagem:{
+    overflow:"visible", 
+    width:"100%",
+    
+    
+  }
 
 
   
