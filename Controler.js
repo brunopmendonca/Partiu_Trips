4e75443c62d69ebe784f5e6d8608b1b968c1754f
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2021-06-07 11:17:16 -0300

    conectando em servidor online

--- Controler.js
+++ Controler.js
@@ -24,7 +24,7 @@ app.post("/login", async (req, res) => {
         where: { name: req.body.name, password: req.body.password }
     })
 
-    let resposta = JSON.parse(response)
+    let resposta = JSON.stringify(response)
     res.send(JSON.parse(resposta))
 
 })
