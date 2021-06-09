d57c6ba28b1a323e6dce06e181dae9e9d01746ae
Bruno César Piovezan Mendonça <bcm_1993@hotmail.com>
2021-06-09 10:16:52 -0300

    conectando em servidor online

--- Controler.js
+++ Controler.js
@@ -14,13 +14,13 @@ let user = models.User
 let viagem = models.Viagem
 let gasto = models.Gasto
 
-app.use("/", express.static(
-    resolve(
-        __dirname,
-        './build'
-    )
+// app.use("/", express.static(
+//     resolve(
+//         __dirname,
+//         './build'
+//     )
 
-))
+// ))
 
 app.get('/', () => {
     res.send("servidor funcionando")
